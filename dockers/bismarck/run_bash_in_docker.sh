4e65c5ff533737b4d9f7c0e5603b16ecb3c6fc26
huahang <huahang@xiaomi.com>
2019-02-02 15:24:50 +0800

    fix a typo
    Signed-off-by: huahang <huahang@xiaomi.com>

--- dockers/bismarck/run_bash_in_docker.sh
+++ dockers/bismarck/run_bash_in_docker.sh
@@ -4,5 +4,5 @@ useradd $DOCKER_USER -b /my_dockers/users -m -u $DOCKER_UID
 echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$DOCKER_USER
 mkdir -p /my_dockers/users/$DOCKER_USER/.ssh
 cp /my_dockers/ssh_config/* /my_dockers/users/$DOCKER_USER/.ssh/
-chown -R $DOCKER_USER . /my_dockers/users/$DOCKER_USER/.ssh/
+chown -R $DOCKER_USER /my_dockers/users/$DOCKER_USER/.ssh/
 /bin/bash /my_dockers/switch_user_in_docker.sh
